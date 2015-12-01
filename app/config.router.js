38db0dc5aca0602951b5990fb863875f18223158
penghaozhong <penghaozhong@163.com>
2015-12-01 11:02:24 +0800

    导入前端文件

---
+++ app/config.router.js
@@ -0,0 +1,107 @@
+﻿'use strict';
+angular.module('app')
+    .run(
+        [
+            '$rootScope', '$state', '$stateParams',
+            function($rootScope, $state, $stateParams) {
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }
+        ]
+    )
+    .config(
+        [
+            '$stateProvider', '$urlRouterProvider',
+            function($stateProvider, $urlRouterProvider) {
+
+                $urlRouterProvider
+                    .otherwise('/app/dashboard');
+                $stateProvider
+                    .state('app', {
+                        abstract: true,
+                        url: '/app',
+                        templateUrl: 'views/layout.html'
+                    })
+                    .state('app.dashboard', {
+                        url: '/dashboard',
+                        templateUrl: 'views/dashboard.html',
+                        ncyBreadcrumb: {
+                            label: '仪表盘',
+                            description: ''
+                        },
+                        resolve: {
+                            deps: [
+                                '$ocLazyLoad',
+                                function($ocLazyLoad) {
+                                    return $ocLazyLoad.load({
+                                        serie: true,
+                                        files: [
+                                            'lib/jquery/charts/sparkline/jquery.sparkline.js',
+                                            'lib/jquery/charts/easypiechart/jquery.easypiechart.js',
+                                            'lib/jquery/charts/flot/jquery.flot.js',
+                                            'lib/jquery/charts/flot/jquery.flot.resize.js',
+                                            'lib/jquery/charts/flot/jquery.flot.pie.js',
+                                            'lib/jquery/charts/flot/jquery.flot.tooltip.js',
+                                            'lib/jquery/charts/flot/jquery.flot.orderBars.js',
+                                            'app/controllers/dashboard.js',
+                                            'app/directives/realtimechart.js'
+                                        ]
+                                    });
+                                }
+                            ]
+                        }
+                    })
+                    
+                    .state('app.databoxes', {
+                        url: '/databoxes',
+                        templateUrl: 'views/databoxes.html',
+                        ncyBreadcrumb: {
+                            label: 'Databoxes',
+                            description: 'beyond containers'
+                        },
+                        resolve: {
+                            deps: [
+                                '$ocLazyLoad',
+                                function($ocLazyLoad) {
+                                    return $ocLazyLoad.load({
+                                        serie: true,
+                                        files: [
+                                            'app/directives/realtimechart.js',
+                                            'lib/jquery/charts/flot/jquery.flot.js',
+                                            'lib/jquery/charts/flot/jquery.flot.orderBars.js',
+                                            'lib/jquery/charts/flot/jquery.flot.tooltip.js',
+                                            'lib/jquery/charts/flot/jquery.flot.resize.js',
+                                            'lib/jquery/charts/flot/jquery.flot.selection.js',
+                                            'lib/jquery/charts/flot/jquery.flot.crosshair.js',
+                                            'lib/jquery/charts/flot/jquery.flot.stack.js',
+                                            'lib/jquery/charts/flot/jquery.flot.time.js',
+                                            'lib/jquery/charts/flot/jquery.flot.pie.js',
+                                            'lib/jquery/charts/morris/raphael-2.0.2.min.js',
+                                            'lib/jquery/charts/chartjs/chart.js',
+                                            'lib/jquery/charts/morris/morris.js',
+                                            'lib/jquery/charts/sparkline/jquery.sparkline.js',
+                                            'lib/jquery/charts/easypiechart/jquery.easypiechart.js',
+                                            'app/controllers/databoxes.js'
+                                        ]
+                                    });
+                                }
+                            ]
+                        }
+                    })
+                    .state('login', {
+                        url: '/login',
+                        templateUrl: 'views/login.html',
+                        ncyBreadcrumb: {
+                            label: 'Login'
+                        }
+                    })
+                    .state('app.dlwzfb', {
+                        url: '/dlwzfb',
+                        templateUrl: 'views/dlwzfb.html',
+                        ncyBreadcrumb: {
+                            label: '地理位置分布'
+                        }
+                    });
+            }
+        ]
+    );
